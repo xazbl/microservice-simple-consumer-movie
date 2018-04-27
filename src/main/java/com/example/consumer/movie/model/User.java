d5a74f25c90f24cfb7b3a6af28ff75db404343ee
blong.zhang <blong.zhang@haihangyun.com>
2018-04-27 09:25:23 +0800

    first commit

---
+++ src/main/java/com/example/consumer/movie/model/User.java
@@ -0,0 +1,61 @@
+package com.example.consumer.movie.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class User implements Serializable {
+    private Long id;
+    private String username;
+    private String name;
+    private Integer age;
+    private String balance;
+    private Date createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
